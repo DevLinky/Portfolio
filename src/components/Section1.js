bacf30e197bfd8f19e77ccd275005a7e2b539e14
DevLinky <86600420+DevLinky@users.noreply.github.com>
2022-05-03 16:17:57 +0200

    Update Section1.js

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -10,22 +10,8 @@ const Section1 = () => {
             <Navigation />
             <section className="section1">
                 <h1 className="title">DevLinky</h1>
-                {/* <div className="text"><h2>Hi, I'm a freelance developper</h2></div>
-                <Typed
-                    strings={["WEB DEV", "REACT FAN", "GEEK"]}
-                    typeSpeed={70}
-                    backSpeed={70}
-                    backDelay={1000}
-                    loop
-                    smartBackspace
-                    loop >
-                    <input type="text" disabled />
-                </Typed> */}
                 <br />
                 <SocialNetwork />
-                {/* <div className="imageContainer">
-                    <img src={logo} alt="logo" className="logo" />
-                </div> */}
                 <br />
                 <button>CONTACT</button>
                 <TypedMessage />
