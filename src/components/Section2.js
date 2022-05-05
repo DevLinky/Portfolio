4fcc2ef3965f0176a8196d6f2f30e84ff00d8992
DevLinky <xpog.devlinky@gmail.com>
2022-05-05 15:31:12 +0200

    lil' update

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -6,7 +6,7 @@ const Section2 = (props) => {
         <>
         <Navigation language={props.language} setLanguage={props.setLanguage} langFile={props.langFile} />
         <section className="section2">
-        <h2 className="skills-text">SKILLS</h2>
+        <h2 className="skills-text">{props.langFile.navigation.skills}</h2>
             <div className="skills-container">
                 
                 <Skill language={props.language}/>
